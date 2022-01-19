d805a243acc5ffbe0b8080810fad5807e7ec043d
James Wickenden <jameswickenden14@gmail.com>
2022-01-19 18:49:17 +0000

    colour setting

--- lifx.py
+++ lifx.py
@@ -7,6 +7,7 @@ headers = {
     "Authorization": "Bearer %s" % token,
 }
 
+
 def update_light_power(is_on):
     # is_on: Boolean controlling light power.
     # a light set to 'off' will not emit light regardless of power, but can still receive updates
@@ -15,19 +16,69 @@ def update_light_power(is_on):
     }
 
     response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+    print("updating power setting to", is_on)
+    print(response,response.json())
     return response
 
 
-def set_state(brightness):
-    # set a state. format: hue, saturation, kelvin, and brightness
+def cycle_states():
+    # doesn't work.
     payload = {
-        "brightness": brightness
+        "states": [{
+                "brightness": 1.0
+            },
+            {
+                "brightness": 0.5
+            },
+            {
+                "brightness": 0.1
+            },
+            {
+                "power": "off"
+            }
+        ],
+        "defaults": {
+            "power": "on",
+            "saturation": 0,
+            "duration": 2.0
+        }
+    }
+    
+    response = requests.post('https://api.lifx.com/v1/lights/all/cycle', data=payload, headers=headers)
+    print("cycling next state...")
+    print(response,response.json())
+    return response
+
+
+def update_temperature(temperature):
+    # set temperature, given in kelvin.
+    # doing so sets saturation to 0.0, effectively clearing any colour values
+    payload = {
+        "color": f"kelvin:{temperature}"
     }
 
     response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+    print("updating temperature...")
+    print(response,response.json())
     return response
 
-response = update_light_power(True)
 
-print(response)
-print(response.json())
+def update_colour(rgb_hex, brightness, duration):
+    # set a state. format: hue, saturation, and brightness
+    # duration: transition time in seconds
+    # brightness: light brightness from 0..1
+    
+    payload = {
+        "color": rgb_hex,
+        "brightness": brightness,
+        "duration": duration,
+        "infrared": 0
+    }
+    response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+    print("updating colour...")
+    print(response,response.json())
+    return response
+
+
+update_light_power(True)
+update_colour("pink", 1.0, 0.5)
