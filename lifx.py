9f49172fa0ed54a683f6a821cc372d6ecf283b87
James Wickenden <jameswickenden14@gmail.com>
2022-01-19 18:00:20 +0000

    started making stuff with api!

--- lifx.py
+++ lifx.py
@@ -7,9 +7,27 @@ headers = {
     "Authorization": "Bearer %s" % token,
 }
 
-payload = {
-    "power": "off",
-}
+def update_light_power(is_on):
+    # is_on: Boolean controlling light power.
+    # a light set to 'off' will not emit light regardless of power, but can still receive updates
+    payload = {
+        "power": {True:"on", False:"off"}.get(is_on, "off")
+    }
 
-response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+    response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+    return response
+
+
+def set_state(brightness):
+    # set a state. format: hue, saturation, kelvin, and brightness
+    payload = {
+        "brightness": brightness
+    }
+
+    response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+    return response
+
+response = update_light_power(True)
+
+print(response)
 print(response.json())
