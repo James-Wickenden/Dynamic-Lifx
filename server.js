c5ad50711776541a009abad8cd4f37f53b697647
James Wickenden <jameswickenden14@gmail.com>
2022-02-02 22:49:51 +0000

    backend db work

--- server.js
+++ server.js
@@ -12,50 +12,141 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
+var db = {};
+
 setup_socket_io();
 setup_express();
 
-
 http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    log('INFO', `Listening on http://localhost:${PORT}`);
+    console.log(`Listening on http://localhost:${PORT}`);
 });
 
 
+// create a file if it doesn't exist
+function create_if_not_exists(filename, content) {
+    try {
+        if (!fs.existsSync(filename)) {
+          fs.writeFile(filename, content, err => {
+            if (err) {
+                log('ERROR', err);
+            }
+        });
+        }
+      } catch(err) {
+        log('ERROR', err);
+      }
+};
+
+// ensure the database file exists, as well as a logging file
+function create_db_log() {
+    create_if_not_exists('db.json', '{}');
+    create_if_not_exists('out.log', new Date(Date.now()).toString() + '\n');
+};
+
+
+// Log msg with corresponding metadata
+function log(level, msg) {
+    var now = new Date(Date.now()).toString();
+    var log_str = level + ' ' + now + ' ' + msg + '\n';
+
+    fs.appendFile('out.log', log_str, function (err) {
+        log('ERROR', err);
+      });
+};
+
+
+// set up express server, its directory, and its filepaths
 function setup_express() {
     // Sets up the express server
     app.use(express.static(path.join(__dirname, 'public')))
     app.get('/', (req, res) => {
             res.sendFile(path.join(__dirname, '/public/index.html'));
         });
+    app.get('/log', (req, res) => {
+        res.sendFile(path.join(__dirname, '/out.log'));
+    });
+    app.get('/db', (req, res) => {
+        res.sendFile(path.join(__dirname, '/db.json'));
+    });
+};
+
+
+// Sets up the socket.io events and handlers
+function setup_socket_io() {
+    io.on('connection', (socket) => {
+        //socket.on('disconnect', () => {
+        //    console.log(socket.id + ' disconnected');
+        //});
+        socket.on('colourChangeRequest', (data) => {
+            log('INFO', 'request by ' + socket.id + ': ' + data.toString());
+            var response = requestColourUpdate(socket.id, data['username'], data['rgb']);
+            if (response) {
+                db[data['username']].push([ Date.now(), socket.id, data['rgb'] ]);
+                rewrite_DB();
+                update_colour(data['rgb'], 1.0, 1.0);
+            }
+            io.to(socket.id).emit('colourChangeResponse', response);
+        });
+    });
+};
+
+
+// read a file and return the text
+function readFile(filename) {
+    try {
+        const data = fs.readFileSync(filename, 'utf8');
+        return data;
+    } catch (err) {
+        log('ERROR', error);
+    }
+};
+
+
+// write to a filename
+function writeFile(filename, content) {
+    try {
+        fs.writeFileSync(filename, content)
+      } catch (err) {
+        log('ERROR', error);
+      }
+};
+
+
+// Reload the JSON database from file into memory
+function reload_DB() {
+    var db_json = JSON.parse(readFile('db.json'));
+    db = db_json;
+};
+
+
+// Update the DB and rewrite it to the JSON file.
+function rewrite_DB() {
+    writeFile('db.json', JSON.stringify(db));
 };
 
 
 // Given an object and some optional numerical bounds, check it is a number and that it falls between those bounds
 function validate_number(number, lowerBound=-Infinity, upperBound=Infinity) {
-    if (isNaN(number)) throw Error(number + ' is not numeric');
-    if (number < lowerBound || number > upperBound) throw Error(number + ' outside given boundaries ' + lowerBound + ', ' + upperBound);
+    if (isNaN(number)) log('ERROR', number + ' is not numeric');
+    if (number < lowerBound || number > upperBound) log('ERROR', number + ' outside given boundaries ' + lowerBound + ', ' + upperBound);
     return true;
 };
 
 
+// receive a HTTP request
 function receiveRequest(error, response, body) {
     if (error) {
-        console.error(error);
+        log('ERROR', error);
       } else {
-        console.log(body);
+        log('INFO', body);
       }
 };
 
 
 // Reads ands parses the LIFX API token from a text file
 function get_token() {
-    var token = "empty";
-    try {
-        const data = fs.readFileSync('api_key.txt', 'utf8');
-        token = data;
-    } catch (err) {
-        console.error(err);
-    }
+    var token = readFile('api_key.txt') || 'empty';
     return token;
 };
 
@@ -102,48 +193,28 @@ function update_colour(rgb_hex, brightness, duration) {
 
 
 // todo: rework logging, pull it out of a true/false returning function!
-function requestColourUpdate(username, rgb) {
+function requestColourUpdate(socket_id, username, rgb) {
     if (!username) return false;
-    if (!logs[username]) {
-        logs[username] = [[ Date.now(), rgb ]];
+    reload_DB();
+
+    if (!db[username]) {
+        db[username] = [];
         return true;
     }
     else {
         var userlogs = logs[username];
         var now = Date.now();
-        for (var i = 0; i < userlogs.length; i++) {
-            console.log(now - userlogs[i][0]) ;
-            if (now - userlogs[i][0] < 8000) {
-                console.error(username + " tried to change colour too quickly. Rejected at " + now);
-                rejections.push([username, now, rgb]);
-                return false;
-            }
+        if (now - userlogs[userlogs.length-1][0] < 8000) {
+            log('WARNING', socket_id + ': ' + username + " tried to change colour too quickly, rejected: " + rgb);
+            return false;
         }
-        userlogs.push([ Date.now(), rgb ]);
-        return true;
+        else {
+            return true;
+        }
     }
 };
 
 
 const token = process.env.LIFX_API_KEY || get_token();
 //update_light_power(true);
-const logs = {};
-const rejections = [];
-
-
-// Sets up the socket.io events and handlers
-function setup_socket_io() {
-    io.on('connection', (socket) => {
-        console.log('a user connected');
-        //socket.on('disconnect', () => {
-        //    console.log(socket.id + ' disconnected');
-        //});
-        socket.on('colourChangeRequest', (data) => {
-            console.log(data);
-            var response = requestColourUpdate(data['username'], data['rgb']);
-            if (response) update_colour(data['rgb'], 1.0, 1.0);
-            console.log(logs, rejections);
-            io.to(socket.id).emit('colourChangeResponse', response);
-        });
-    });
-};
+create_db_log();
